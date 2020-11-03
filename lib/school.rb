0d7de5dda5b7715ef9e50d1574f559421415684d
Simran <mdrsimran@gmail.com>
2020-11-02 19:28:23 -0600

    commit tree fae8c6e5502b057614ccde99331410d988d1895f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
 # code here!
+class School
+
+    attr_accessor :roster, :name
+    
+
+    def initialize(name)
+        @name=name
+        @roster={}
+    end
+
+    def add_student(student, level)
+        roster[level] ||= []
+        roster[level] << student
+    end
+
+    def grade(level)
+        roster[level]
+        # roster.detect do |x,y|
+        #     if x == level
+        #         return y
+        #     end
+        # end
+    end
+
+    def sort
+        new_list={}
+        roster.each do |x,y|
+            new_list[x] = y.sort
+        end
+        new_list
+    end
+
+    # def roster(name, grade)
+    #     @name= roster[:@name]
+    #     @grade= roster[:@grade]
+    # end
+
+    # def add_student(name,grade)
+    #     @add_student
+    # end
+end
